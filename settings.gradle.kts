f19c79a9e6077ee24a59584bf49616a00e71d81e
Shawlaw <Shawlaw@126.com>
2024-11-02 16:51:08 +0800

    Opt: 添加腾讯云、阿里云、华为云等镜像源优化项目在网络环境不佳时的初始化效率

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 pluginManagement {
     repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin/")
+        }
+        maven {
+            url = uri("https://repo.huaweicloud.com/repository/maven/")
+        }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,6 +20,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/central/")
+        }
+        maven {
+            url = uri("https://repo.huaweicloud.com/repository/maven/")
+        }
         google()
         mavenCentral()
     }
